e9bea900f01670243cf63af1d95b466176a22c4a
Anthony McGrath <akm@anthonykyle.co.nz>
2020-08-06 19:35:38 +1200

    Complete Get Neighbours, and countAliveNeigbours

--- countAliveNeighbours.js
+++ countAliveNeighbours.js
@@ -1,7 +1,9 @@
 const getNeighbours = require('./getNeighbours')
 
 function countAliveNeighbours (cellRow, cellColumn, board) {
-
+  let neighbours = getNeighbours(cellRow, cellColumn, board);
+  neighbours = neighbours.filter(neighbour => { return neighbour === true; });
+  return neighbours.length
 }
 
 module.exports = countAliveNeighbours
--- getNeighbours.js
+++ getNeighbours.js
@@ -1,20 +1,16 @@
 const indicesAreOutOfBounds = require('./indicesAreOutOfBounds')
 
 function getNeighbours (cellRow, cellColumn, board) {
-    let count = 0;
-    let value = 0;
+    let neighbours = [];
     for(let y = cellRow-1; y <= cellRow+1; y++){
         for(let x = cellColumn-1; x <= cellColumn+1; x++){
-            // console.log(x,y)
             if(!indicesAreOutOfBounds(x, y, board) && !(x === cellColumn && y === cellRow)){
-                count++
-                value += board[y][x];
+                
+                neighbours.push(board[y][x]);
             }
         }
     }
-    console.log(count)
-    console.log(value)
-    return [count, value];
+    return neighbours;
 }
 
 module.exports = getNeighbours
